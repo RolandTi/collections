10ea58a189467b9bb1b73f979e01c24afe25cbf8
Roland <roland.pistache@gmail.com>
2022-06-03 12:00:04 +0200

    Fixed issue with functions.php

--- collections/functions.php
+++ collections/functions.php
@@ -1,8 +1,7 @@
 <?php
-zp_register_filter('themeSwitcher_head', 'switcher_head');
-zp_register_filter('themeSwitcher_Controllink', 'switcher_controllink');
-zp_register_filter('theme_head', 'css_head', 500);
+#zp_register_filter('themeSwitcher_head', 'switcher_head');
+#zp_register_filter('themeSwitcher_Controllink', 'switcher_controllink');
+#zp_register_filter('theme_head', 'css_head', 500);
 #enableExtension('zenpage', 0, false); //	we do not support it
-
 $_zp_page_check = 'checkPageValidity'; //	opt-in, standard behavior
 ?>
--- collections/theme_description.php
+++ collections/theme_description.php
@@ -1,7 +1,7 @@
 <?php
 $theme_description['name'] = 'Collections';
 $theme_description['author'] = 'Roland - https://github.com/RolandTi/collections';
-$theme_description['version'] = '1.0';
-$theme_description['date'] = '26/03/21';
+$theme_description['version'] = '0.6.1';
+$theme_description['date'] = '03/06/22';
 $theme_description['desc'] = 'Collections is a responsive theme for Zenphoto image gallery and CMS.';
 ?>
