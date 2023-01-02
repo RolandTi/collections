c8280aea630e7075100bf05b15c8691c405efd00
Roland <roland.pistache@gmail.com>
2023-01-02 09:33:50 +0100

    Updated version and date

--- collections/theme_description.php
+++ collections/theme_description.php
@@ -2,6 +2,6 @@
 $theme_description['name'] = 'Collections';
 $theme_description['author'] = 'Roland Ti - <a href="https://github.com/RolandTi/collections">GitHub</a>';
 $theme_description['version'] = '1.1';
-$theme_description['date'] = '17/12/22';
+$theme_description['date'] = '02/01/23';
 $theme_description['desc'] = 'Collections is a responsive theme for ZenphotoCMS.';
 ?>
