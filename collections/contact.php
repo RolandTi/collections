997658f1226c5b55e0c891e34094e666fb7f4541
Roland <roland.pistache@gmail.com>
2023-03-23 12:30:47 +0100

    Moved header in _header.php

--- collections/contact.php
+++ collections/contact.php
@@ -2,14 +2,7 @@
 <!doctype html>
 <html<?php printLangAttribute(); ?>>
 <head>
-	<meta http-equiv='X-UA-Compatible' content='IE=edge'>
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<meta charset="<?php echo LOCAL_CHARSET; ?>">
-	<?php zp_apply_filter('theme_head'); ?>
-	<?php printHeadTitle(); ?>
-	<?php if (class_exists('RSS')) printRSSHeaderLink('Gallery', gettext('Gallery RSS')); ?>
-	<link rel="stylesheet" href="<?php echo $_zp_themeroot; ?>/styles.css" type="text/css" />
-	 <script>const colorScheme = localStorage.getItem("color-scheme") || "light-mode";</script>
+	<?php include("_header.php"); ?>
 </head>
 <body>
 		<?php zp_apply_filter('theme_body_open'); ?>
